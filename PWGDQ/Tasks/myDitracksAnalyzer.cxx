ce05e5f19b52b062cd7bb9792d5a709a3f87edf0
sigurd <sigurdnese@gmail.com>
2025-05-19 12:01:48 -0700

    Add simple task to loop over ditracks table

---
+++ PWGDQ/Tasks/myDitracksAnalyzer.cxx
@@ -0,0 +1,52 @@
+// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
+//
+// This software is distributed under the terms of the GNU General Public
+// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+///
+/// \brief This task is an empty skeleton that fills a simple eta histogram.
+///        it is meant to be a blank page for further developments.
+/// \author everyone
+
+#include <fairlogger/Logger.h>
+#include "Framework/runDataProcessing.h"
+#include "PWGDQ/DataModel/ReducedInfoTables.h"
+#include "Framework/AnalysisTask.h"
+
+using namespace o2;
+using namespace o2::framework;
+
+struct myDitracksAnalyzer {
+  // Histogram registry: an object to hold your histograms
+  HistogramRegistry histos{"histos", {}, OutputObjHandlingPolicy::AnalysisObject};
+  // Map to track how many times an event has been encountered
+  std::map<int32_t, int8_t> fEventCount;
+
+  void init(InitContext const&)
+  {
+    // define axes you want to use
+    // const AxisSpec axisEta{30, -1.5, +1.5, "#eta"};
+
+    // create histograms
+    // histos.add("etaHistogram", "etaHistogram", kTH1F, {axisEta});
+  }
+
+  void process(aod::Ditracks::iterator const& ditrack)
+  {
+    if (fEventCount.find(ditrack.reducedeventId()) != fEventCount.end()) {
+      LOGF(info, "!!! This event (%d) has been encountered %d times before", ditrack.reducedeventId(), fEventCount[ditrack.reducedeventId()]);
+    }
+    fEventCount[ditrack.reducedeventId()] += 1;
+  }
+};
+
+WorkflowSpec defineDataProcessing(ConfigContext const& cfgc)
+{
+  return WorkflowSpec{
+    adaptAnalysisTask<myDitracksAnalyzer>(cfgc)};
+}
